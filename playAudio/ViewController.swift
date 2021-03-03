f75bdd598cd0cec1f7b77b208f1016d34919b213
廖昱晴 <pulin.liao@ctbcsec.com>
2021-03-03 11:00:18 +0800

    init audioPlayer

--- playAudio/ViewController.swift
+++ playAudio/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var audioPlayer: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let path = Bundle.main.path(forResource: "Right", ofType: "mp3") { //音檔路徑
+            let url = URL(fileURLWithPath: path) //將路徑轉換成URL
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: url) //AVAudioPlayer會丟錯誤訊息(throw)，所以要包在do catch內並前面要加try
+                audioPlayer?.enableRate = true //可以調整播放速度，預設為1
+                audioPlayer?.rate = 2.0 //播放速度調為2
+                audioPlayer?.numberOfLoops = 2 //播放次數，預設為0(播一次)，-1為一直無限重播
+                audioPlayer?.volume = 0.3 //音量大小，預設為1
+            } catch {
+                
+            }
+        }
     }
 
-
+    @IBAction func play(_ sender: UIButton) {
+        audioPlayer?.stop() //停止所有播放
+        audioPlayer?.currentTime = 0 //音檔播放時間軸為0
+        audioPlayer?.play() //播放音檔
+    }
+    
 }
 
