9ba04f573fb6bc6f8588de1b0a11392c126d7b4d
廖昱晴 <pulin.liao@ctbcsec.com>
2021-03-03 11:09:47 +0800

    改用guard let重新撰寫

--- playAudio/ViewController.swift
+++ playAudio/ViewController.swift
@@ -14,18 +14,33 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let path = Bundle.main.path(forResource: "Right", ofType: "mp3") { //音檔路徑
-            let url = URL(fileURLWithPath: path) //將路徑轉換成URL
-            do {
-                audioPlayer = try AVAudioPlayer(contentsOf: url) //AVAudioPlayer會丟錯誤訊息(throw)，所以要包在do catch內並前面要加try
-                audioPlayer?.enableRate = true //可以調整播放速度，預設為1
-                audioPlayer?.rate = 2.0 //播放速度調為2
-                audioPlayer?.numberOfLoops = 2 //播放次數，預設為0(播一次)，-1為一直無限重播
-                audioPlayer?.volume = 0.3 //音量大小，預設為1
-            } catch {
-                
-            }
+//        if let path = Bundle.main.path(forResource: "Right", ofType: "mp3") { //音檔路徑
+//            let url = URL(fileURLWithPath: path) //將路徑轉換成URL
+//            do {
+//                audioPlayer = try AVAudioPlayer(contentsOf: url) //AVAudioPlayer會丟錯誤訊息(throw)，所以要包在do catch內並前面要加try
+//                audioPlayer?.enableRate = true //可以調整播放速度，預設為1
+//                audioPlayer?.rate = 2.0 //播放速度調為2
+//                audioPlayer?.numberOfLoops = 2 //播放次數，預設為0(播一次)，-1為一直無限重播
+//                audioPlayer?.volume = 0.3 //音量大小，預設為1
+//            } catch { // 發生錯誤時執行
+//                print(error.localizedDescription)
+//            }
+//        } else {
+//            print("no such file")
+//        }
+        
+        guard let newPath = Bundle.main.path(forResource: "Right", ofType: "mp3") else { //確保有值，若找不到進入else並不再執行下面的程式碼(return)
+            print("no such file")
+            return
         }
+        
+        let url = URL(fileURLWithPath: newPath)
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+        } catch {
+            print(error.localizedDescription)
+        }
+        
     }
 
     @IBAction func play(_ sender: UIButton) {
